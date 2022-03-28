3f2220953a1ce161d6049c5768eb55122e3a4cd8
GreenWizard <sizeof.2011@gmail.com>
2022-03-28 15:32:42 +0300

    Impl. returning links (Core\CMonitoringTask.py : 4) | 12d 18h

--- Core/CMonitoringTask.py
+++ Core/CMonitoringTask.py
@@ -1,8 +1,11 @@
 class CMonitoringTask:
+  def __init__(self, links):
+    self._links = links
+    return
+  
   @property
   def links(self):
-    # TODO: Impl. returning links
-    return []
+    return self._links
   
   def update(self, jobs):
     # TODO: Impl. sending jobs to callback & refreshing task last update time
